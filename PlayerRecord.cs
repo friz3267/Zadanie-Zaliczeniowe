edf9ab2cbfee9945894a9965e6bbc304934d297f
friz3267 <mikolajpaszka@gmail.com>
2026-06-14 15:13:46 +0200

    Update PlayerRecord.cs

--- PlayerRecord.cs
+++ PlayerRecord.cs
@@ -2,8 +2,6 @@ namespace ZgadnijLiczbe
 {
     public class PlayerRecord
     {
-        // ENKAPSULACJA: Użycie właściwości z modyfikatorem 'private set'. 
-        // Chroni to dane przed modyfikacją z zewnątrz po tym, jak obiekt zostanie już stworzony.
         public string Name { get; private set; }
         public int Tries { get; private set; }
         public Difficulty GameDifficulty { get; private set; }
