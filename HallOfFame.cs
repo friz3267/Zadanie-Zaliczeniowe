d0ff75e103995b1c36df4fc71705fb050be3cde8
friz3267 <mikolajpaszka@gmail.com>
2026-06-14 15:12:53 +0200

    Remove unnecessary comments and clean up code

--- HallOfFame.cs
+++ HallOfFame.cs
@@ -6,9 +6,6 @@ namespace ZgadnijLiczbe
 {
     public class HallOfFame
     {
-        // ENKAPSULACJA: Lista przechowująca rekordy jest prywatna. 
-        // Nie pozwalamy nikomu z zewnątrz bezczelnie w niej grzebać.
-        // Modyfikacja listy jest możliwa tylko poprzez publiczne metody tej klasy (AddRecord, Clear).
         private List<PlayerRecord> _records = new List<PlayerRecord>();
 
         public void AddRecord(PlayerRecord record)
@@ -31,7 +28,7 @@ namespace ZgadnijLiczbe
             var top5 = _records
                 .Where(r => r.GameDifficulty == diff)
                 .OrderBy(r => r.Tries)
-                .ThenBy(r => r.DurationSeconds) // Sortowanie przy remisach próbowych rozstrzygane na podstawie czasu
+                .ThenBy(r => r.DurationSeconds) 
                 .Take(5)
                 .ToList();
 
